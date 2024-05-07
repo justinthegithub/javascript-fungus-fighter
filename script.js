b1205718a3a06bea87b9923fba1ae530391a7180
Justin Kahn <71735000+justinthegithub@users.noreply.github.com>
2024-05-07 10:31:00 +0000

    outline

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
-
+let fungusHP=100;
+let fungusAp=100;
 function onReady() {
     console.log("Ready to go!")
     
@@ -15,6 +16,31 @@ function onReady() {
     // - Updates state which is ->
     // - Rendered to the DOM
 }
+//update state variable(s) to make the Freaky Fungus lose hit points //(HP), and to reduce your attack points (AP).
+/*Attacks
+arcane scepterArcane Scepter
+AP Cost: 12
+HP Damage: 14
+entangleEntangle
+AP Cost: 23
+HP Damage: 9
+dragon bladeDragon Blade
+AP Cost: 38
+HP Damage: 47
+star fireStar Fire
+AP Cost: 33
+HP Damage: 25
 
+*/
+
+
+function loseHitPoints(attacks) {
+
+
+}
+
+function reduceAttackPoints(attacks){
+
+}
 
 onReady()
